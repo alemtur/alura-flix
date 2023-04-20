f78ca22e3ca83131ee7c14004e2211708757d964
ale <alejandro.turtola@gmail.com>
2023-04-20 20:43:55 -0300

    Styled Components added y boton creado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 /* import logo from './logo.svg';
 import './App.css'; */
+import { Boton } from "./Components/Boton";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +18,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <Boton>Normal</Boton>
+      <Boton $primary>Primary</Boton>
+
+
     </div>
   );
 }
